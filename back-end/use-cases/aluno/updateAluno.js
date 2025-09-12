0dd287615b41209bfb7c2aa54b06ccd64f5535e3
Micael Lima <224446543+micaeljlima@users.noreply.github.com>
2025-09-11 21:06:02 -0300

    reorganizando o projeto

---
+++ back-end/use-cases/aluno/updateAluno.js
@@ -0,0 +1,35 @@
+const calcularIdade = require('../../utils/aluno/calcularIdade');
+const validarEmail = require('../../utils/aluno/validarEmail');
+const { atualizarAluno } = require('../../services/alunoService');
+
+module.exports = async (id, novosDados) => {
+  // Validação de email
+  if (novosDados.email && !validarEmail(novosDados.email)) {
+    throw { status: 400, message: 'Email inválido' };
+  }
+
+  // Validação de idade e dados do responsável
+  if (novosDados.data_nascimento) {
+    const idade = calcularIdade(novosDados.data_nascimento);
+
+    if (isNaN(idade)) {
+      throw { status: 400, message: 'Data de nascimento inválida. Use o formato DD/MM/AAAA.' };
+    }
+
+    if (idade < 18 && (!novosDados.nome_responsavel || !novosDados.email_responsavel || !novosDados.telefone_responsavel)) {
+      throw { status: 400, message: 'Dados do responsável são obrigatórios para menores de idade' };
+    }
+
+    novosDados.idade = idade;
+    novosDados.nome_responsavel = idade < 18 ? novosDados.nome_responsavel : null;
+    novosDados.email_responsavel = idade < 18 ? novosDados.email_responsavel : null;
+    novosDados.telefone_responsavel = idade < 18 ? novosDados.telefone_responsavel : null;
+  }
+
+  const sucesso = await atualizarAluno(id, novosDados);
+  if (!sucesso) {
+    throw { status: 404, message: 'Aluno não encontrado' };
+  }
+
+  return { message: 'Aluno atualizado com sucesso' };
+};
